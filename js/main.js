eb72f18b0817e843a597704a84d16efdb62ec126
Laura Escalante <laura088.le@gmail.com>
2023-12-10 19:54:12 -0300

    add show/hide fiters

--- js/main.js
+++ js/main.js
@@ -17,9 +17,23 @@ const closeMenu = () =>{
     $("main").classList.remove("translate-y-32")
 }
 
+// Filtros
+const showFilters = () =>{
+    hideElement("#hide-filters")
+    showElement("#show-filters")
+    hideElement("#filters")
+}
+const hideFilters = () =>{
+    showElement("#hide-filters")
+    hideElement("#show-filters")
+    showElement("#filters")
+}
+
 const initializeProject = () => {
     $("#open-menu").addEventListener("click", openMenu)
     $("#close-menu").addEventListener("click", closeMenu)
+    $("#hide-filters").addEventListener("click",showFilters)
+    $("#show-filters").addEventListener("click",hideFilters)
 }
 
 window.addEventListener("load", initializeProject)
